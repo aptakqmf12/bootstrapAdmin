0294a535f66d2ec7cc1dddb1d7ba6e70495c0d7a
aptakqmf12 <leexodhks12@naver.com>
2021-11-27 21:50:52 +0900

    axios추가

--- src/views/base/notice/Notice.js
+++ src/views/base/notice/Notice.js
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import Pagination from 'rc-pagination'
 import {
   CCard,
   CCardBody,
@@ -18,6 +20,31 @@ import {
 
 const Notice = () => {
   const [data, setData] = useState([])
+  const [value, setValue] = useState({
+    ID: 0,
+    SUBJECT: '23',
+    CONTENT: '43',
+    CRADTED: new Date(),
+    MODIFIED: new Date(),
+  })
+
+  const onChangeInput = (e) => {}
+
+  const onSubmitForm = (e) => {
+    e.preventDefault()
+    alert('add')
+    setData([
+      ...data,
+      {
+        //post 보낼땐 camel케이스로.
+        ID: data.length + 1,
+        SUBJECT: 1,
+        CONTENT: 2,
+        CRADTED: new Date(),
+        MODIFIED: new Date(),
+      },
+    ])
+  }
 
   const styleCenter = {
     display: 'flex',
@@ -25,69 +52,83 @@ const Notice = () => {
   }
 
   useEffect(() => {
-    fetch('http://localhost:3005/api/notice/list', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        id: 1,
-      }),
-    })
-      .then((res) => res.json())
-      .then((json) => setData(json.data))
+    async function fetchData() {
+      const res = await axios.get('http://localhost:3005/api/notice/list')
+      setData(res.data.data)
+    }
+    return fetchData()
   }, [])
+
   return (
-    <CCard>
-      <CCardHeader>
-        <strong>게시판</strong> <small>Fetch</small>
-      </CCardHeader>
-      <CCardBody>
-        <CTable striped>
-          <CTableHead>
-            <CTableRow>
-              <CTableHeaderCell scope="col">id</CTableHeaderCell>
-              <CTableHeaderCell scope="col">SUBJECT</CTableHeaderCell>
-              <CTableHeaderCell scope="col">CONTENT</CTableHeaderCell>
-              <CTableHeaderCell scope="col">CREATED</CTableHeaderCell>
-              <CTableHeaderCell scope="col">MODIFIED</CTableHeaderCell>
-            </CTableRow>
-          </CTableHead>
+    <>
+      <CCard>
+        <CCardHeader>
+          <strong>게시판</strong> <small>Fetch</small>
+        </CCardHeader>
+        <CCardBody>
+          <CTable striped>
+            <CTableHead>
+              <CTableRow>
+                <CTableHeaderCell scope="col">id</CTableHeaderCell>
+                <CTableHeaderCell scope="col">SUBJECT</CTableHeaderCell>
+                <CTableHeaderCell scope="col">CONTENT</CTableHeaderCell>
+                <CTableHeaderCell scope="col">CREATED</CTableHeaderCell>
+              </CTableRow>
+            </CTableHead>
 
-          <CTableBody>
-            {data.map((e, i) => {
-              return (
-                <>
-                  <CTableRow>
-                    <CTableDataCell key={e.ID}>{e.ID}</CTableDataCell>
-                    <CTableDataCell key={e.SUBJECT}>{e.SUBJECT}</CTableDataCell>
-                    <CTableDataCell key={e.CONTENT}>{e.CONTENT}</CTableDataCell>
-                    <CTableDataCell key={e.CREATED}>{e.CREATED}</CTableDataCell>
-                    <CTableDataCell key={e.MODIFIED}>{e.MODIFIED}</CTableDataCell>
-                  </CTableRow>
-                </>
-              )
-            })}
-          </CTableBody>
-        </CTable>
+            <CTableBody>
+              {data.map((e, i) => {
+                return (
+                  <>
+                    <CTableRow key={e.id}>
+                      <CTableDataCell>{e.ID}</CTableDataCell>
+                      <CTableDataCell>{e.SUBJECT}</CTableDataCell>
+                      <CTableDataCell>{e.CONTENT}</CTableDataCell>
+                      <CTableDataCell>{e.CREATED}</CTableDataCell>
+                    </CTableRow>
+                  </>
+                )
+              })}
+            </CTableBody>
+          </CTable>
 
-        <CPagination aria-label="Page navigation example" style={styleCenter}>
-          <CPaginationItem aria-label="Previous">
-            <span aria-hidden="true">&laquo;</span>
-          </CPaginationItem>
-          {data.map((e, i) => {
-            return (
-              <>
-                <CPaginationItem>{i + 1}</CPaginationItem>
-              </>
-            )
-          })}
-          <CPaginationItem aria-label="Next">
-            <span aria-hidden="true">&raquo;</span>
-          </CPaginationItem>
-        </CPagination>
-      </CCardBody>
-    </CCard>
+          <CPagination aria-label="Page navigation example" style={styleCenter}>
+            <CPaginationItem aria-label="Previous">
+              <span aria-hidden="true">&laquo;</span>
+            </CPaginationItem>
+
+            <CPaginationItem aria-label="Next">
+              <span aria-hidden="true">&raquo;</span>
+            </CPaginationItem>
+          </CPagination>
+        </CCardBody>
+      </CCard>
+      <div>
+        <form onSubmit={onSubmitForm}>
+          <fieldset>
+            <legend>게시글 추가</legend>
+
+            <label htmlFor={'CONTENT'}>게시글 CONTENT</label>
+            <input
+              name="content"
+              className="CONTENT"
+              value={value.CONTENT}
+              onChange={onChangeInput}
+            ></input>
+
+            <label htmlFor={'SUBJECT'}>게시글 SUBJECT</label>
+            <input
+              name="subject"
+              className="SUBJECT"
+              value={value.SUBJECT}
+              onChange={onChangeInput}
+            ></input>
+
+            <button type="submit">추가!</button>
+          </fieldset>
+        </form>
+      </div>
+    </>
   )
 }
 export default Notice
